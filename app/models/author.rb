3ff0e54bda251a4f8b7476e66d8eb382f169c56a
Matteo Piccini <gnappo.1@live.it>
2023-01-31 16:16:25 -0800

    commit tree 6583768fcf49d7f7d6b5a2952568d97f2db57d4f

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: {case_sensitive: false}
+    validates :phone_number, length: {is: 10, message: "%{value} is not 10 digits, come on dude!"}
+    # validates_presence_of :name
 end
